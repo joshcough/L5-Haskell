f3787e92f35655742df201062460f64e19d3922b
joshcough <joshcough@gmail.com>
2014-08-06 17:53:45 -0400

    fixed up executables

--- executables/LExecNative.hs
+++ executables/LExecNative.hs
@@ -7,7 +7,7 @@ import L.L2.L2
 import L.L3.L3
 
 main = withFileArg $ \file -> g (getExtension file) (getDir file) file where
-  go lang dir file = compileAndRunNativeFile lang dir file >> return ()
+  go lang dir file = compileAndRunNativeFile lang dir file >>= putStrLn . runVal
   g "L1" = go l1Language
   g "L2" = go l2Language
   g "L3" = go l3Language
