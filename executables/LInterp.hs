0058b9e18270f5ffafba9f2f8f2db731a436b8c1
joshcough <joshcough@gmail.com>
2014-08-06 17:35:50 -0400

    reorganizing executables

---
+++ executables/LInterp.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import L.Compiler
+import L.IOHelpers
+import L.L1.L1
+import L.L2.L2
+import L.L3.L3
+
+main = withFileArg $ \file -> g (getExtension file) file where
+  go lang file = interpretFile lang file >> return ()
+  g "L1" = go l1Language
+  g "L2" = go l2Language
+  g "L3" = go l3Language
--- mains/l2/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main (main) where
-
-import L.Compiler
-import L.IOHelpers
-import L.L2.L2 
-
-main = withFileArg $ \f -> 
-  compileFileAndWriteResult l2Language (getDir f) f >> return ()
