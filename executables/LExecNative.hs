d1bc228cb798170d8909a6523d94b764bfe98dfa
joshcough <joshcough@gmail.com>
2014-09-15 20:20:16 -0400

    minor fixes for OS support

--- executables/LExecNative.hs
+++ executables/LExecNative.hs
@@ -1,16 +1,19 @@
 module Main where
 
+import L.CommandLine
 import L.Compiler
 import L.IOHelpers
 import L.L1.L1
 import L.L2.L2
 import L.L3.L3
+import Options.Applicative
 
-import System.Info as Info
+main :: IO ()
+main = execParser commandLineParser >>= main'
 
-main = withFileArg $ \file -> g (getExtension file) (getDir file) file where
-  go lang dir file = compileAndRunNativeFile lang opts dir file >>= putStrLn . runVal
+main' :: (CompilationOptions, FilePath) -> IO ()
+main' (opts, file) = g (getExtension file) (getDir file) where
+  go lang dir = compileAndRunNativeFile lang opts dir file >>= putStrLn . runVal
   g "L1" = go l1Language
   g "L2" = go l2Language
   g "L3" = go l3Language
-  opts = CompilationOptions { L.Compiler.os = error "todo" }
--- src/L/OS.hs
+++ src/L/OS.hs
@@ -1,9 +1,11 @@
 module L.OS where
 
+import System.Info as Sys
+
 data OS = Darwin | Linux deriving (Show, Eq)
 
 osFromMaybeString :: Maybe String -> OS
-osFromMaybeString = maybe Darwin osFromString
+osFromMaybeString = maybe (osFromString $ Sys.os) osFromString
 
 osFromString :: String -> OS
 osFromString "darwin" = Darwin
