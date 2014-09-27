2ae4210f54fefec565089fd0db309ed02d7eef6d
joshcough <joshcough@gmail.com>
2014-09-27 00:53:30 -0400

    fixed a bug where error messages were being swallowed.

--- executables/LC.hs
+++ executables/LC.hs
@@ -6,6 +6,7 @@ import L.IOHelpers
 import L.L1.L1
 import L.L2.L2
 import L.L3.L3
+import L.L4.L4
 import Options.Applicative
 
 main :: IO ()
@@ -13,8 +14,11 @@ main = execParser commandLineParser >>= main'
 
 main' :: (CompilationOptions, FilePath) -> IO ()
 main' (opts, file) = g (getExtension file) (getDir file) where
-  go lang dir = compileFileAndWriteResult lang opts dir file >> return ()
+  go lang dir = do
+    res <- compileFileAndWriteResult lang opts dir file
+    case res of { Left err -> error err; Right _  -> return () }
   g "L1" = go l1Language
   g "L2" = go l2Language
   g "L3" = go l3Language
+  g "L4" = go l4Language
 
