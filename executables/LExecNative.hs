266f76a331cbef4f05f14a2f27998206c46c5710
joshcough <joshcough@gmail.com>
2014-09-27 00:54:05 -0400

    added L4.

--- executables/LExecNative.hs
+++ executables/LExecNative.hs
@@ -6,6 +6,7 @@ import L.IOHelpers
 import L.L1.L1
 import L.L2.L2
 import L.L3.L3
+import L.L4.L4
 import Options.Applicative
 
 main :: IO ()
@@ -16,4 +17,4 @@ main' (opts, file) = g (getExtension file) (getDir file) where
   go lang dir = compileAndRunNativeFile lang opts dir file >>= putStrLn . runVal
   g "L1" = go l1Language
   g "L2" = go l2Language
-  g "L3" = go l3Language
+  g "L4" = go l4Language
--- executables/LInterp.hs
+++ executables/LInterp.hs
@@ -7,6 +7,7 @@ import L.L1.L1Interp
 import L.L1.L1
 import L.L2.L2
 import L.L3.L3
+import L.L4.L4
 import Options.Applicative
 
 main :: IO ()
@@ -19,3 +20,4 @@ main' (opts, file) = g (getExtension file) (getDir file) where
   g "L1" = go l1Language
   g "L2" = go l2Language
   g "L3" = go l3Language
+  g "L4" = go l4Language
