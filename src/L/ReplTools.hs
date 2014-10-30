e2eca86a2d206ffa861b12ae54d9222d2cc94ab7
joshcough <joshcough@gmail.com>
2014-10-30 11:49:38 -0400

    added quickInterp to ReplTools, fixed up a res file that was broken.

--- src/L/ReplTools.hs
+++ src/L/ReplTools.hs
@@ -6,6 +6,7 @@
 module L.ReplTools (
    quickCompile
   ,quickCompileTurtles
+  ,quickInterp
   ,quickRunNative
   ,showFile
 ) where
@@ -18,6 +19,7 @@ import L.L1.L1
 import L.L2.L2
 import L.L3.L3
 import L.L4.L4
+import L.L5.L5
 import System.FilePath.Lens
 
 opts :: CompilationOptions
@@ -30,6 +32,7 @@ quickCompile inputFile = g $ inputFile^.extension where
   g ".L2" = compileFileAndWriteResult l2Language opts inputFile >>= return . show
   g ".L3" = compileFileAndWriteResult l3Language opts inputFile >>= return . show
   g ".L4" = compileFileAndWriteResult l4Language opts inputFile >>= return . show
+  g ".L5" = compileFileAndWriteResult l5Language opts inputFile >>= return . show
   g _    = error $ "Error: bad L file: " ++ inputFile
 
 quickCompileTurtles :: FilePath -> IO ()
@@ -38,6 +41,7 @@ quickCompileTurtles inputFile = g $ inputFile^.extension where
   g ".L2" = compileTurtlesFile l2Language opts inputFile
   g ".L3" = compileTurtlesFile l3Language opts inputFile
   g ".L4" = compileTurtlesFile l4Language opts inputFile
+  g ".L5" = compileTurtlesFile l5Language opts inputFile
   g _    = error $ "Error: bad L file: " ++ inputFile
 
 quickRunNative :: Show o => FilePath -> IO String
@@ -46,6 +50,15 @@ quickRunNative inputFile = g $ inputFile^.extension where
   g ".L2" = compileFileAndRunNative l2Language opts inputFile >>= return . show
   g ".L3" = compileFileAndRunNative l3Language opts inputFile >>= return . show
   g ".L4" = compileFileAndRunNative l4Language opts inputFile >>= return . show
+  g _    = error $ "Error: bad L file: " ++ inputFile
+
+quickInterp :: Show o => FilePath -> IO (Either String String)
+quickInterp inputFile = g $ inputFile^.extension where
+  g ".L1" = interpretFile l1Language inputFile
+  g ".L2" = interpretFile l2Language inputFile
+  g ".L3" = interpretFile l3Language inputFile
+  g ".L4" = interpretFile l4Language inputFile
+  g ".L5" = interpretFile l5Language inputFile
   g _    = error $ "Error: bad L file: " ++ inputFile
 
 showFile :: FilePath -> IO ()
